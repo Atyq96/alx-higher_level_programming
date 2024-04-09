37bd38ffa7de05799a021605b464ed1518223208
Atyq96 <atyqamine1996@gmail.com>
2024-04-09 02:31:07 +0000

    done

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 // Script that prints 3 lines
 
-console.log('C is fun\nPython is cool\nJavascript is amazing');
+const lines = 'C is fun\nPython is cool\nJavascript is amazing';
+console.log(lines);
