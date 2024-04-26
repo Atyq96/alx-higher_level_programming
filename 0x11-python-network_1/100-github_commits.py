d9f357712736a73ee9449124178b4f4fb2b43406
Atyq96 <atyqamine1996@gmail.com>
2024-04-26 06:08:49 -0700

    update

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,8 +6,6 @@ sent in as arguments
 if __name__ == '__main__':
     import requests
     from sys import argv
-
-
     r = requests.get('https://api.github.com/repos/{}/{}/commits'
                     .format(argv[2], argv[1]))
     commits = r.json()
